6b6e024e1e61c22f21d46a00d6a977035c95cd76
caposcar1998 <oscarcontrerascap@gmail.com>
2020-06-30 20:26:37 -0500

    Se agrega flowrouter al rpoeycto

--- SmartKidsActivities/imports/startup/client/routes.js
+++ SmartKidsActivities/imports/startup/client/routes.js
@@ -1,17 +1,17 @@
-import { FlowRouter } from 'meteor/kadira:flow-router'
 import React from 'react'
+import { FlowRouter } from 'meteor/kadira:flow-router'
 import { mount } from 'react-mounter'
-
 //Layouts and Pages
 
 import App from '../../ui/App';
+import PaginaInicio from '../../ui/PaginaInicio/PaginaInicio';
 
 
 FlowRouter.route('/', {
-    name: 'login',
+    name: 'inicio',
     action() {
         mount(App, {
-            content: <Login />
+            content: <PaginaInicio />
         })
     }
 })
--- SmartKidsActivities/imports/ui/App.jsx
+++ SmartKidsActivities/imports/ui/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-export const App = () => (
-  <div>
-    <h1>Hola</h1>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  fondoPagina: {
+    backgroundColor: "white"
+  }
+}));
+
+export default function App(props) {
+  const classes = useStyles();
+
+
+
+  return (
+    <div>
+
+      {props.content}
+
+    </div>
+
+  );
+}
